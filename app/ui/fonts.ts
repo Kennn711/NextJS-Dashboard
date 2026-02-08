ec06beea25072e2f96ed88a0b93c38e9a5346813
Kennn <marcell040507@gmail.com>
2026-02-08 21:01:14 +0700

    chapter 3 of nextJS lesson

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { DM_Sans } from "next/font/google";
+import { Lexend } from "next/font/google";
+
+export const dmSans = DM_Sans({ subsets: ["latin"] });
+export const lexend = Lexend({ weight: ["300", "400"], subsets: ["latin"] });
